f8c79987d0ab9a1eb2372fdb5e17b3e8aba41462
jfemon8 <jfemon8@gmail.com>
2025-02-02 17:26:01 +0600

    Complete main assignment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['InterVariable', '...defaultTheme.fontFamily.sans'],
+            }
+        },
+    },
+    // ...
+
+}
